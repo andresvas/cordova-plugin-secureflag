8b532401ef2a50f58306526905ef958738f3876b
andres vasquez <andresvas1995@outlook.com>
2022-08-09 17:53:57 -0500

    Update SecureFlag.java

--- src/android/SecureFlag.java
+++ src/android/SecureFlag.java
@@ -1,5 +1,9 @@
 package cordova.plugin.secureflag;
 
+import android.app.Activity;
+import android.app.Application;
+import android.os.Build;
+import android.os.Bundle;
 import android.view.WindowManager;
 
 import org.apache.cordova.CordovaInterface;
@@ -13,7 +17,7 @@ import org.json.JSONException;
 /**
  * This class add secure flag when applications is in background. (ActivityLifecycleCallbacks)
  */
-public class SecureFlag extends CordovaPlugin {
+public class SecureFlag extends CordovaPlugin implements Application.ActivityLifecycleCallbacks {
 
     private static boolean activeFlag = false;
 
@@ -21,6 +25,9 @@ public class SecureFlag extends CordovaPlugin {
     public void initialize(CordovaInterface cordova, CordovaWebView webView) {
         super.initialize(cordova, webView);
         activeFlag = false;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            cordova.getActivity().registerActivityLifecycleCallbacks(this);
+        }
     }
 
     @Override
@@ -39,13 +46,68 @@ public class SecureFlag extends CordovaPlugin {
         return false;
     }
 
+    @Override
+    public void onResume(boolean multitasking) {
+        super.onResume(multitasking);
+        if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.Q) {
+            cordova.getActivity().getWindow().clearFlags(WindowManager.LayoutParams.FLAG_SECURE);
+        }
+        //cordova.getActivity().getWindow().clearFlags(WindowManager.LayoutParams.FLAG_SECURE);
+    }
 
     @Override
     public void onPause(boolean multitasking) {
-        if (activeFlag) {
-            cordova.getActivity().getWindow().addFlags(WindowManager.LayoutParams.FLAG_SECURE);
-        }
+        // if (activeFlag) {
+        cordova.getActivity().getWindow().addFlags(WindowManager.LayoutParams.FLAG_SECURE);
+        // }
         super.onPause(multitasking);
     }
 
+    @Override
+    public void onActivityCreated(Activity activity, Bundle savedInstanceState) {
+
+    }
+
+    @Override
+    public void onActivityStarted(Activity activity) {
+
+    }
+
+    @Override
+    public void onActivityResumed(Activity activity) {
+
+    }
+
+    @Override
+    public void onActivityPrePaused(Activity activity) {
+        activity.getWindow().setFlags(WindowManager.LayoutParams.FLAG_SECURE, WindowManager.LayoutParams.FLAG_SECURE);
+
+    }
+
+
+    @Override
+    public void onActivityPaused(Activity activity) {
+        activity.getWindow().setFlags(WindowManager.LayoutParams.FLAG_SECURE, WindowManager.LayoutParams.FLAG_SECURE);
+    }
+
+    @Override
+    public void onActivityPostResumed(Activity activity) {
+        activity.getWindow().clearFlags(WindowManager.LayoutParams.FLAG_SECURE);
+
+    }
+
+    @Override
+    public void onActivityStopped(Activity activity) {
+
+    }
+
+    @Override
+    public void onActivitySaveInstanceState(Activity activity, Bundle outState) {
+
+    }
+
+    @Override
+    public void onActivityDestroyed(Activity activity) {
+
+    }
 }
